0677fec52605c2f6b716f03c42dafcea5cf1226d
nurasliaarifah29 <asliaarifa55@gmail.com>
2026-09-02 23:28:48 +0800

    Day001.java
    Updated multi-line comment to include additional lines.

--- Day001.java
+++ Day001.java
@@ -2,6 +2,9 @@ public class Days1 {
     public static void main(String[] args) {
         System.out.println("Hari ini adalah hari pertama");
         // Simbol ini digunakan untuk 1 baris
-        /* Simbol ini digunakan untuk beberapa baris */
+        /* Simbol untuk beberapa baris 
+        ini adalah baris kedua
+        ini adalah baris ketiga
+        */
     }
 }
