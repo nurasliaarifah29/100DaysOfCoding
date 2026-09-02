c8e2acf74d3992d5ae7716dbae053b6256c78f67
nurasliaarifah29 <asliaarifa55@gmail.com>
2026-09-02 23:08:10 +0800

    Days001.java

---
+++ Day001.java
@@ -0,0 +1,7 @@
+public class Days1 {
+    public static void main(String[] args) {
+        System.out.println("Hari ini adalah hari pertama");
+        // Simbol ini digunakan untuk 1 baris
+        /* Simbol ini digunakan untuk beberapa baris */
+    }
+}
