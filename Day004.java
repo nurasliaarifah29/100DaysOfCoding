023f8dda13f9f993df23fe9a4dd349f3f48fd439
nurasliaarifah29 <asliaarifa55@gmail.com>
2026-09-05 18:47:56 +0800

    Day004.java
    This Java program demonstrates the use of various numeric data types including int, byte, long, and short.

---
+++ Day004.java
@@ -0,0 +1,17 @@
+public class day4 {
+    public static void main(String[] args) {
+        // Tipe Data Numerik Bilangan Bulat
+        int jumlahBuku = 10;
+        byte anak_ke = 3;
+        long jumlahUang = 500000000L;
+        short tahunMasuk = 2025;
+        System.out.println("Jumlah buku saya : " + jumlahBuku);
+        System.out.println("Saya anak ke : " + anak_ke);
+        System.out.println("jumlah uang saya : " + jumlahUang);
+        System.out.println("Saya masuk kuliah pada tahun : " + tahunMasuk);
+        //int digunakan untuk menyimpan bilangan bulat
+       //byte digunakan untuk bilangan bulat yang nilainya relatif kecil
+       //long digunakan untuk menyimpan bilangan bulat yang besar
+      //short digunakan untuk menyimpan bilangan bulat dengan ukuran lebih kecil daripada int
+    }
+}
