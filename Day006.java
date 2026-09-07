00eb6eb1fa0d3d56a68abd1025bbf4909475b4e6
nurasliaarifah29 <asliaarifa55@gmail.com>
2026-09-07 21:47:32 +0800

    Day006.java
    Implement main method demonstrating char and boolean data types.

---
+++ Day006.java
@@ -0,0 +1,13 @@
+public class day6 {
+    public static void main(String[] args) {
+        //Tipe Data Karakter (char) dan Boolean(boolean)
+        char kelas = 'C';
+        boolean sehat = true;
+        System.out.println("Kelas : " + kelas);
+        System.out.println("Sehat : " + sehat); 
+        //Tipe data char digunakan untuk karakter tunggal
+        /*Tipe data boolean digunakan untuk merepresentasikan kondisi logis yang hanya
+        memiliki dua kemungkinan, yaitu True atau False, Benar atau Salah
+        */
+    }
+}
