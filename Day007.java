cd25540acc03147a12f194044afac097b4b66198
nurasliaarifah29 <asliaarifa55@gmail.com>
2026-09-08 22:24:55 +0800

    Day007.java
    This program prints student information including name, ID, faculty, and program.

---
+++ Day007.java
@@ -0,0 +1,14 @@
+public class day7 {
+    public static void main(String[] args) {
+        String nama = "lia";
+        String nim = "D0225023";
+        String fakultas = "Teknik";
+        String prodi = "Informatika";
+        System.out.println("nama : " + nama);
+        System.out.println("nim : " + nim);
+        System.out.println("fakultas : " + fakultas);
+        System.out.println("prodi : " + prodi);
+        /*String digunakan untuk teks atau kalimat, String juga termasuk tipe data non primitif
+        dan menggunakan tanda kutip*/
+    }
+}
