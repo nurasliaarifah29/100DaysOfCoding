be09c0393c9a2b6235d6853bfdc518ab609b2574
nurasliaarifah29 <asliaarifa55@gmail.com>
2026-09-09 19:39:14 +0800

    Day008.java
    The variable 'nama' is updated from 'a'ba' to 'mama', and 'umur' is changed from 45 to 40.

---
+++ Day008.java
@@ -0,0 +1,18 @@
+public class day8 {
+    public static void main(String[] args) {
+    String nama; //Deklarasi variabel adalah proses mendefinisikan variabel dengan menggunakan tipe data dan nama variabel
+    int umur;
+
+    nama = "a'ba"; //Inisialisasi variabel adalah proses memberikan nilai awal kepada variabel yang telah dideklarasikan
+    umur = 45;
+
+    nama = "mama"; //Update variabel adalah proses mengubah nilai variabel yang telah ada
+    umur = 40;
+
+    System.out.println(nama);
+    System.out.println(umur);
+
+
+
+    }
+}
