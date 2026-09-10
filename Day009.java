62262c05c42fab654a92d83b65149520db31e48b
nurasliaarifah29 <asliaarifa55@gmail.com>
2026-09-10 22:51:43 +0800

    Day009.java
    Added a Java class with a main method that prints a constant value.

---
+++ Day009.java
@@ -0,0 +1,10 @@
+public class day9 {
+    public static void main(String[] args) {
+        final int umur = 19;
+        System.out.println(umur);
+        /*Constanta (Final Variabel) adalah variabel yang nilainya tidak dapat diubah
+        setelah di inisialisasi.Variabel ini di deklarasikan dengan menggunakan kata kunci final 
+        dan biasanya digunakan untuk menyimpan nilai yang tidak berubah sepanjang program.*/
+    } 
+    }
+
