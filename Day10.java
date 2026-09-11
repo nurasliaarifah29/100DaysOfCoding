d48cfccb06926b5f4e8a0e7193ddbc5d74caedfb
nurasliaarifah29 <asliaarifa55@gmail.com>
2026-09-11 23:23:57 +0800

    Day10.java

---
+++ Day10.java
@@ -0,0 +1,30 @@
+public class day10 {
+    public static void main(String[] args) {
+        System.out.println("Batas Minimal dan Maksimal Tipe Data");
+
+        System.out.println("byte  : " + Byte.MIN_VALUE + " sampai " + Byte.MAX_VALUE);
+        //Menampilkan batas minimal dan maksimal tipe data byte
+
+        System.out.println("short : " + Short.MIN_VALUE + " sampai " + Short.MAX_VALUE);
+        //Menampilkan batas minimal dan maksimal tipe data short
+
+        System.out.println("int   : " + Integer.MIN_VALUE + " sampai " + Integer.MAX_VALUE);
+        //Menampilkan batas minimal dan maksimal tipe data int
+
+        System.out.println("long  : " + Long.MIN_VALUE + " sampai " + Long.MAX_VALUE);
+        //Menampilkan batas minimal dan maksimal tipe data int
+
+         System.out.println("float  : " + Float.MIN_VALUE + " sampai " + Float.MAX_VALUE);
+        //Menampilkan batas minimal dan maksimal tipe data float
+
+         System.out.println("double  : " + Double.MIN_VALUE + " sampai " + Double.MAX_VALUE);
+        //Menampilkan batas minimal dan maksimal  tipe data double
+
+        System.out.println("char : " + (int) Character.MIN_VALUE + " sampai " + (int) Character.MAX_VALUE);
+        //Menampilkan batas minimal dan maksimal char dalam bentuk angka 
+
+        System.out.println("boolean : " + Boolean.FALSE+ " dan " + Boolean.TRUE);
+        //Menampilkan nilai yang dimiliki tipe data boolean (false dan true)
+    }
+}
+
