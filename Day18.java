2e96252118a41ab082c1dfe8c28490c96a931a3a
nurasliaarifah29 <asliaarifa55@gmail.com>
2026-09-19 18:35:59 +0800

    Day18.java

---
+++ Day18.java
@@ -0,0 +1,20 @@
+public class day18{
+    public static void main(String[] args) {
+    
+    //Konversi Otomatis (Dari tipe data kecil ke besar
+        //int ke long 
+        int nilaiA = 80;
+        long nilaiB = nilaiA;
+        System.out.println("int ke long : " + nilaiB);
+
+        //int ke float
+        int nilaiC = 70;
+        float nilaiD = nilaiC;
+        System.out.println("int ke float : " + nilaiD);
+
+        //int ke double 
+        int nilaiE = 60;
+        double nilaiF = nilaiE;
+        System.out.println("int ke double : " + nilaiF);
+    }
+}
