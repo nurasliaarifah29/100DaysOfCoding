9baade8b2e01fc2c156daded8a4dfd179c49bcbf
nurasliaarifah29 <asliaarifa55@gmail.com>
2026-09-20 21:21:43 +0800

    Day19.java
    Added a Java program demonstrating manual type conversion from long, double, and float to int.

---
+++ Day19.java
@@ -0,0 +1,21 @@
+public class day19{
+    public static void main(String[] args) {
+
+        //Konversi manual/paksa (Dari tipe data besar ke kecil)
+
+            //long ke int 
+            long nilaiE = 200;
+            int nilaiF = (int) nilaiE;
+            System.out.println("long ke int : " + nilaiE);
+
+            //double ke int
+            double nilaiA = 85.5;
+            int nilaiB = (int) nilaiA;
+            System.out.println("double ke int : " + nilaiB);
+
+            //float ke int 
+            float nilaiC = 72.8f;
+            int nilaiD = (int) nilaiC;
+            System.out.println("float ke int : " + nilaiC);         
+}
+}
