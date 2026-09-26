e64582860c0cb75276f612e41a59014519603c09
nurasliaarifah29 <asliaarifa55@gmail.com>
2026-09-26 21:05:06 +0800

    Day25.java

--- Day25.java
+++ Day25.java
@@ -2,12 +2,20 @@ import java.util.Scanner;
 
 public class day25 {
     public static void main(String[] args) {
-        
-        //program menghitung luas lingkaran
+
         Scanner s = new Scanner(System.in);
-        System.out.print("Masukkan jari-jari lingkaran : ");
-        double r = s.nextDouble();
-        double luas = Math.PI * r * r;
-        System.out.println("Luas lingkaran = " + luas);
+
+        System.out.print("Masukkan jari-jari: ");
+        String input = s.nextLine();
+
+        int r = Integer.parseInt(input);
+        double jariJari = r;
+
+        double luas = 3.14 * jariJari * jariJari;
+        double hasil = luas - (luas * 2);
+
+        String hasilString = String.valueOf(hasil);
+
+        System.out.printf("Hasil akhir = %s%n", hasilString);
     }
 }
